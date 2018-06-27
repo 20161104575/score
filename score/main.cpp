9d153f6e37812f1f2fdfd971db60c81e7879284c
20161104575 <1975950726@qq.com>
2018-06-27 09:43:54 +0800

    score
    wenjianduqu

--- score/main.cpp
+++ score/main.cpp
@@ -7,9 +7,36 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+string Trim(string& str)
+{
+    str.erase(0,str.find_first_not_of("\t\r\n"));
+    str.erase(str.find_last_not_of("\t\r\n")+1);
+    return str;
+}
+int main()
+{
+    ifstream fin("/Users/a20161104575/Desktop/scoresort/score/score.csv ");
+    
+    string line;
+    while (getline(fin, line)) {
+        //cout << line << endl;
+        
+        istringstream sin(line);
+        vector<string> fields;
+        string field;
+        while (getline(sin, field, ',')) {
+            fields.push_back(field);
+        }
+        string name = Trim(fields[0]);
+        string age = Trim(fields[1]);
+        string birthday = Trim(fields[2]);
+        cout << name << "\t" << age << "\t" << birthday << endl; 
+    }
 }
